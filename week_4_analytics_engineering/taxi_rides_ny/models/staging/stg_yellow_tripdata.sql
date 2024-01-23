d272c9202d14a57188621e5f0dca08674e136c86
Hoang Hai Pham <binarsenal02011997@gmail.com>
2024-01-23 21:26:39 +0000

    fixed error for deployment

--- week_4_analytics_engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
+++ week_4_analytics_engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
@@ -37,7 +37,7 @@ select
     cast(improvement_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
     cast((payment_type+1) as integer) as payment_type,
-    {{ get_payment_type_description('payment_type', 'yellow_tripdata') }} as payment_type_description,
+    {{ get_payment_type_description('payment_type') }} as payment_type_description,
     cast(congestion_surcharge as numeric) as congestion_surcharge
 from tripdata
 where rn = 1
